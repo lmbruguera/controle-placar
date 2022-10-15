76e2594c843037bbabc647f0ea5dbbeb82cee3c1
lmbruguera <lmbruguera@gmail.com>
2022-10-15 17:55:39 -0300

    Now it has control of time and show to the user parcial time and total time

--- controle-placar.ino
+++ controle-placar.ino
@@ -34,22 +34,39 @@ int voltaCompleta = 0;
 
 unsigned long tInicial = 0;
 unsigned long tFinal = 0;
-unsigned long tParcial = 0;
-unsigned long tTotal = 0;
 unsigned long tRefresh = 0;
 unsigned long tAtual = 0;
+unsigned long timeNow = 0;
+unsigned long timeBefore = 0;
+unsigned long diffTime = 0;
+unsigned long diffTotal = 0;
+
+int segundoParcial = 0;
+int minutoParcial = 0;
+int horaParcial = 0;
+int segundoCompleto = 0;
+int minutoCompleto = 0;
+int horaCompleto = 0;
+
+
+String tTotal;
+String tParcial;
+
 
 String longTimeStr(const time_t &t){
   
   // Retorna segundos como "d:hh:mm:ss"
-  
-  String s = String(t / SECS_PER_DAY) + ':';
-  if (hour(t) < 10) {
-    s += '0';
+  String s = "";
+//  String s = String(t / SECS_PER_DAY) + ':';
+//  if (hour(t) < 10) {
+//    s += '0';
+//  }
+//  s += String(hour(t)) + ':';
+
+  if (minute(t) < 1) {
+    s += "0";
   }
-  s += String(hour(t)) + ':';
-  
-  if (minute(t) < 10) {
+  else if(minute(t) >= 1 && minute(t) < 10){
     s += '0';
   }
   s += String(minute(t)) + ':';
@@ -72,16 +89,16 @@ void printDisplay(){
   display.clearDisplay();
   delay(10);
   oledPrint("Voltas totais:", 5, 5);
-  oledPrint("Tempo total:", 5, 15);
+  oledPrint("T. tot:", 5, 15);
   oledPrint("Voltas parciais:", 5, 25);
-  oledPrint("Tempo parcial:", 5, 35);
+  oledPrint("T. parc:", 5, 35);
 
-  oledPrint(String(voltaCompleta), 105, 5);
-  oledPrint(String(tTotal), 105, 15);
+  oledPrint(String(voltaCompleta), 100, 5);
+  oledPrint(tTotal, 85, 15);
   oledPrint(String(voltaParcial), 105, 25);
-  oledPrint(String(tParcial), 105, 35);
+  oledPrint(tParcial, 85, 35);
 
-  oledPrint(longTimeStr(millis()/1000), 5, 55);
+  oledPrint(longTimeStr(millis()/1000), 80, 55);
   display.display();
 }
 
@@ -89,7 +106,19 @@ void printDisplay(){
 void addScore(uint8_t pinIn){
   voltaParcial++;
   voltaCompleta++;
+
+  Serial.print("Time before: ");
+  Serial.println(timeBefore);
+  diffTime = tAtual - timeBefore;
+  diffTotal += diffTime;
+  timeBefore = tAtual;
+  Serial.print("Time atual: ");
+  Serial.println(tAtual);
+  Serial.print("Diff Time: ");
+  Serial.println(diffTime);
   
+  tParcial = longTimeStr(diffTime);
+  tTotal = longTimeStr(diffTotal);
   printDisplay();
 }
 
@@ -133,7 +162,7 @@ void setup() {
   }
   oledPrint("Olá",0,0);
   display.display();
-  display.setTextSize(1);
+  display.setTextSize(1.5);
   display.setTextColor(WHITE);
 
   delay(1000);
@@ -146,10 +175,13 @@ void setup() {
 void loop() {
 
   unsigned long now = millis();
+  timeNow = millis()/1000;
   tAtual = millis()/1000;
   if(tAtual - tRefresh == 1UL){
     printDisplay();
   }
+
+  
 
   buttonA.process(now);
   buttonB.process(now);
